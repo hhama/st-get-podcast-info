1c18f2345ca553aab926c13816157fca137468e8
hhama <hhama@notus.dti.ne.jp>
2024-07-04 16:17:44 +0900

    rebuild.fm → Rebuild

--- st_get_podcast_info/streamlit_app.py
+++ st_get_podcast_info/streamlit_app.py
@@ -92,7 +92,7 @@ RSS_URL = {
     "上京ボーイズ": "https://anchor.fm/s/11f7ff38/podcast/rss",
     "忘れてみたい夜だから": "https://radiotalk.jp/rss/3710302b24c7e88c",
     "まめまめキャスト": "https://anchor.fm/s/8bec512c/podcast/rss",
-    "rebuild.fm": "https://feeds.rebuild.fm/rebuildfm",
+    "Rebuild": "https://feeds.rebuild.fm/rebuildfm",
     "でこぽんFM": "https://anchor.fm/s/8f913194/podcast/rss",
     "Ossan.fm": "https://ossan.fm/feed.xml",
     "いなみまも": "https://anchor.fm/s/3af273dc/podcast/rss",
