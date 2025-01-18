41a6d8825a6f832f1f3168ffa635ec12e27a67a7
hhama <hhama@notus.dti.ne.jp>
2025-01-18 11:23:37 +0900

    Podcastの一覧辞書を別ファイルにした

---
+++ st_get_podcast_info/podcast_dict.py
@@ -0,0 +1,13 @@
+RSS_URL = {
+    "ゆとりっ娘たちのたわごと": "https://anchor.fm/s/6e491dbc/podcast/rss",
+    "ドングリFM": "https://anchor.fm/s/76a89c80/podcast/rss",
+    "上京ボーイズ": "https://anchor.fm/s/11f7ff38/podcast/rss",
+    "忘れてみたい夜だから": "https://radiotalk.jp/rss/3710302b24c7e88c",
+    "まめまめキャスト": "https://anchor.fm/s/8bec512c/podcast/rss",
+    "Rebuild": "https://feeds.rebuild.fm/rebuildfm",
+    "でこぽんFM": "https://anchor.fm/s/8f913194/podcast/rss",
+    "Ossan.fm": "https://ossan.fm/feed.xml",
+    "いなみまも": "https://anchor.fm/s/3af273dc/podcast/rss",
+    "Sounds by monolith": "https://anchor.fm/s/1b32dd5c/podcast/rss",
+    "いきぬき給湯室": "https://anchor.fm/s/85c06888/podcast/rss",
+}
--- st_get_podcast_info/streamlit_app.py
+++ st_get_podcast_info/streamlit_app.py
@@ -5,6 +5,8 @@ from dateutil.parser import parse
 import feedparser
 import streamlit as st
 
+from podcast_dict import RSS_URL
+
 
 def duration_to_seconds(duration: str) -> int:
     time_data = [int(x) for x in duration.split(":")]
@@ -131,20 +133,6 @@ def output_column(
 
 
 def main() -> None:
-    RSS_URL = {
-        "ゆとりっ娘たちのたわごと": "https://anchor.fm/s/6e491dbc/podcast/rss",
-        "ドングリFM": "https://anchor.fm/s/76a89c80/podcast/rss",
-        "上京ボーイズ": "https://anchor.fm/s/11f7ff38/podcast/rss",
-        "忘れてみたい夜だから": "https://radiotalk.jp/rss/3710302b24c7e88c",
-        "まめまめキャスト": "https://anchor.fm/s/8bec512c/podcast/rss",
-        "Rebuild": "https://feeds.rebuild.fm/rebuildfm",
-        "でこぽんFM": "https://anchor.fm/s/8f913194/podcast/rss",
-        "Ossan.fm": "https://ossan.fm/feed.xml",
-        "いなみまも": "https://anchor.fm/s/3af273dc/podcast/rss",
-        "Sounds by monolith": "https://anchor.fm/s/1b32dd5c/podcast/rss",
-        "いきぬき給湯室": "https://anchor.fm/s/85c06888/podcast/rss",
-    }
-
     st.title("PodcastのRSSをごにょごにょ")
 
     podcast = st.selectbox("Podcastを選択", RSS_URL.keys())
